3c44f585f1807bc0bf64289f87e0662e5081fc52
breannams <breannamarie_s@hotmail.com>
2021-05-26 19:27:49 -0400

    Change code to authenticate password and redirect to login page if not authenticated.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,11 @@ class SessionsController < ApplicationController
     def create
    
         @user = User.find_by(username: params[:username])
-        if @user
+        if @user.authenticate(params[:password])
             session[:user_id] = @user.id 
             redirect_to user_path(@user)
         else 
-            redirect_to new_user_path(@user)
+            redirect_to login_path(@user)
         end
     end
 
