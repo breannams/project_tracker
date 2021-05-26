a77f4d3792a7e3933d8e6ac4eb75145291c89092
breannams <breannamarie_s@hotmail.com>
2021-05-26 19:25:36 -0400

    Add functionality to sessions methods and create form for loging in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,21 @@
+
 class SessionsController < ApplicationController
     def home
     end
 
     def new
-      
+      @user = User.new
     end
 
     def create
-
+   
+        @user = User.find_by(username: params[:username])
+        if @user
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            redirect_to new_user_path(@user)
+        end
     end
 
 end
