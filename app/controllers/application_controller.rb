7c2bc61cd18dce91aacff14ddcaa8e5e38c9e1b7
breannams <breannamarie_s@hotmail.com>
2021-05-26 19:02:23 -0400

    Add methods for current user sessions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action: require_log_in, except: [:new, :create, :home]
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_log_in
+        if !logged_in?
+            redirect_to root_path
+        end
+    end
 end
