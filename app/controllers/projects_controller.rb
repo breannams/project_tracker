85edfc352bd98cc4b0eb9e3a83bac5d2a0c9e987
breannams <breannamarie_s@hotmail.com>
2021-05-26 20:23:20 -0400

    Create initial methods and views for projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,28 @@
 class ProjectsController < ApplicationController
+    before_action :require_log_in
+
+    def index
+    end
+
+    def new
+    end
+
+    def create 
+    end
+
+    def show
+
+    end
+
+    def create
+    end
+
+    def edit
+    end
+    
+    def update
+    end
+
+    def destroy
+    end
 end
