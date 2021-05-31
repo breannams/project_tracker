ee61d08762d5ad8db242a413d175a734d88d809e
breannams <breannamarie_s@hotmail.com>
2021-05-31 17:55:21 -0400

    Add functionality to create a new entry.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,32 @@
 class EntriesController < ApplicationController
+    before_action :require_log_in
+    before_action :set_entry, only:[:edit, :update, :destroy]
+    
+    def new
+        @entry = Entry.new
+    end
+
+    def create
+        @entry = current_user.entries.create(entry_params)
+        if @entry.save
+            redirect_to :controller => "projects", :action => "index"
+        else
+            render :new
+        end
+    end
+
+    def edit
+
+    end
+
+
+    private
+    
+    def set_entry
+        @entry = Entry.find_by_id(params[:id])
+    end
+
+    def entry_params
+        params.require(:entry).permit(:notes, :project_id)
+    end
 end
