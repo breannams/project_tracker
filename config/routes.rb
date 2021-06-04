402a52f1a38a28dfa6108a59c30a296d9986c3d7
breannams <breannamarie_s@hotmail.com>
2021-06-04 14:33:24 -0400

    Add an index route, action & view for categories. Change some of layout on category showpage.

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,6 @@ Rails.application.routes.draw do
     resources :projects, only: [:index]
   end
 
-  resources :categories, only: [:show]
+  resources :categories, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
